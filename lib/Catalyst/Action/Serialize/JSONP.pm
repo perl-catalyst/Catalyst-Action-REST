610e03b2a87dafc4da0d192c882b75999891ad45
Tomas Doran <bobtfish@bobtfish.net>
2010-01-14 20:52:10 +0000

    Trim trailing whitespace

--- lib/Catalyst/Action/Serialize/JSONP.pm
+++ lib/Catalyst/Action/Serialize/JSONP.pm
@@ -4,14 +4,14 @@ use namespace::autoclean;
 
 extends 'Catalyst::Action::Serialize::JSON';
 
-after 'execute' => sub { 
+after 'execute' => sub {
   my $self = shift;
   my ($controller, $c) = @_;
 
   my $callback_key = (
     $controller->{'serialize'} ?
       $controller->{'serialize'}->{'callback_key'} :
-      $controller->{'callback_key'} 
+      $controller->{'callback_key'}
     ) || 'callback';
 
   my $callback_value = $c->req->param($callback_key);
@@ -19,7 +19,7 @@ after 'execute' => sub {
     if ($callback_value =~ /^\w+$/) {
       $c->res->content_type('text/javascript');
       $c->res->output($callback_value.'('.$c->res->output().');');
-    } else { 
+    } else {
       warn 'Callback: '.$callback_value.' will not generate valid Javascript. Falling back to JSON output';
     }
   }
