4fe67634b658624795bac2b6511263d27ef3e04f
Wallace Reis <wallace@reis.me>
2012-06-11 14:39:04 +0200

    Fix default OPTIONS handler
    As the ->body is defined, then serialization won't happen and we don't
    wrong responses as from failing tests in prev commit.

--- lib/Catalyst/Action/REST.pm
+++ lib/Catalyst/Action/REST.pm
@@ -146,6 +146,7 @@ sub _return_options {
     $c->response->content_type('text/plain');
     $c->response->status(200);
     $c->response->header( 'Allow' => \@allowed );
+    $c->response->body(q{});
 }
 
 sub _return_not_implemented {
